46a1dd4ce8d07ba52e2836483ae30b342e0506c5
Aakash Soni <inos92@gmail.com>
2023-05-07 10:50:54 -0500

    created controllers for sports, bet_types, wagers, users

---
+++ app/controllers/bet_types_controller.rb
@@ -0,0 +1,2 @@
+class BetTypesController < ApplicationController
+end
---
+++ test/controllers/bet_types_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BetTypesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
