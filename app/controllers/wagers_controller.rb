c419c1df0949bd888c39340725035daad29203a6
Aakash Soni <inos92@gmail.com>
2023-05-07 14:13:29 -0500

    added index and create actions for wagers, rails tests passed

--- app/controllers/wagers_controller.rb
+++ app/controllers/wagers_controller.rb
@@ -1,2 +1,23 @@
 class WagersController < ApplicationController
+  def index
+    @wagers = Wager.all
+    render json: @wagers
+  end
+
+  def create
+    @wager = Wager.new(
+      user_id: params[:user_id],
+      bet_type_id: params[:bet_type_id],
+      sport_id: params[:sport_id],
+      wager_amount: params[:wager_amount],
+      odds: params[:odds],
+      win: params[:win],
+      profit_loss: params[:profit_loss],
+    )
+    if @wager.save
+      render json: { message: "Wager saved!" }, status: :created
+    else
+      render json: { errors: @wager.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
--- test/controllers/wagers_controller_test.rb
+++ test/controllers/wagers_controller_test.rb
@@ -4,4 +4,27 @@ class WagersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "index" do
+    get "/wagers.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Wager.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Wager.count", 1 do
+      post "/wagers.json", params: {
+                             user_id: 1,
+                             bet_type_id: 1,
+                             sport_id: 1,
+                             wager_amount: 1.00,
+                             odds: 110,
+                             win: true,
+                             profit_loss: 1.10,
+                           }
+      assert_response 201
+    end
+  end
 end
