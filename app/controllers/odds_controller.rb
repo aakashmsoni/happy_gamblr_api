ab698306d2eed68c2a4a7366f0dca3edec835d89
Aakash Soni <inos92@gmail.com>
2023-05-11 20:03:52 -0500

    created odds controller for Odds API, set up GET req in index action. Encrypted api_key, created routes and tested index using requests.http

---
+++ app/controllers/odds_controller.rb
@@ -0,0 +1,10 @@
+class OddsController < ApplicationController
+  def index
+    # url = URI("https://api.the-odds-api.com/v4/sports/?apiKey=#{Rails.application.credentials.happy_gamblr_api[:api_key]}")
+    url = URI("https://api.the-odds-api.com/v4/sports/basketball_nba/odds/?apiKey=#{Rails.application.credentials.happy_gamblr_api[:api_key]}&regions=us2&markets=h2h&oddsFormat=american")
+    response = Net::HTTP.get_response(url)
+
+    data = JSON.parse(response.body)
+    render json: data.as_json
+  end
+end
---
+++ test/controllers/odds_controller_test.rb
@@ -0,0 +1,4 @@
+require "test_helper"
+
+class OddsControllerTest < ActionDispatch::IntegrationTest
+end
